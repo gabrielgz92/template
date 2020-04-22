24d3cd627a3c399c079d1b5b684f17c988973cc4
gabriel zabala <ggonzalezzabala@gmail.com>
2020-04-22 15:39:05 +1000

    Initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,19 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+import 'assets/javascripts/resume.min.js'
+import 'assets/stylesheets/resume.min.css'
+
+import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/css/all.css'
